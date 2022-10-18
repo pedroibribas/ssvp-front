e713340cef7154fc4422e702fa487cf9ede5f81e
Pedro Ribas <pedroibribas@gmail.com>
2022-10-18 12:19:46 -0400

    feature: add Flyer and Dashboard pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import { Home } from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Dashboard } from "./pages/Dashboard";
+import { Flyer } from "./pages/Flyer";
 import "./styles/global.scss";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/flyer/:id" element={<Flyer />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
